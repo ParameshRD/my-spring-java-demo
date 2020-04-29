4a373b26bba3240e7bb9ead972e5a5dd0dce34e5
parameshrdgowda <parameshrdgowda@gmail.com>
2020-04-29 19:59:31 +0530

    first commit

---
+++ src/main/java/com/myapp/demo/MySpringJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringJavaDemoApplication.class, args);
+	}
+
+}
